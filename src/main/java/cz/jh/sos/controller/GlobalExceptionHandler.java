aacbbd00900c13615dd589d7a82ea980508e565b
JiriNovotny <swat12386@seznam.cz>
2018-12-18 12:20:04 +0100

    prvni commit

---
+++ src/main/java/cz/jh/sos/controller/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package cz.jh.sos.controller;
+
+
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public void handleNonExistingResource() {
+        // empty
+
+
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public void handleIllegalArgument(){
+
+    }
+}
